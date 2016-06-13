474aaa8efe3d8f38b21c0fb7f844eebfbbab8b11
Amixer <aealbaugh@soloprowebsites.com>
2016-06-13 13:34:00 -0400

    initial commit

---
+++ js/app.js
@@ -0,0 +1,37 @@
+$(document).ready(function () { 
+	// This function is to get the data from youtube API and display it on the page
+	function getResults(searchTerm) {
+		$.getJSON("https://www.googleapis.com/youtube/v3/search",
+			{
+				"part": "snippet",
+				"key": "AIzaSyBROXW6-Yk_Na23eOiSrP_G-OSHK3y6lVk",
+				"q": searchTerm 
+			},
+			function (data) {
+				if (data.pageInfo.totalResults == 0) {
+					alert("You Did it! You actually thought of something no one else has! (There are no videos for your search.)");
+				}
+				// If there are no results it will just empty the list
+				displaySearchResults(data.items);
+			}
+
+		);
+	}
+
+	function displaySearchResults(videos) {
+		var html = "";
+		$.each(videos, function (index, video) {
+			// append li to ul
+			console.log(video.snippet.thumbnails.medium.url);
+			html = html + "<li><p>" + video.snippet.title +
+				"</p><img src='" +  video.snippet.thumbnails.high.url + "'/></li>" ;
+
+		});
+		$("#search-results ul").html(html);
+	}
+
+	$("#search-form").submit(function (event) {
+		event.preventDefault();
+		getResults($("#query").val());
+	});
+});
